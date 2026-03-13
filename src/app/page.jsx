0530ad50676abafd2bff301c463575364c3420ac
MATHEUS HENRIQUE DELBELLO OLIVEIRA <matheus.oliveira1233>
2026-03-13 11:45:42 -0300

    estilixacao

---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styles from ".page.modules.css";
+
+function home() {
+    return (
+        <>
+            <div className={"styles.containerListas"}>
+                <h1>exemplos</h1>
+            </div>
+                <div className={"styles.containerListas"}>
+                    <h1>atividades</h1>
+                </div>
+        </>
+    )
+}
+
